b806b87b432ff2688dd42e7e645d1edd35981e6e
Victor Souza <victor.ti.souza@gmail.com>
2020-08-22 16:55:02 -0300

    dynamic route add

--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 
+import '../models/category.dart';
+
 class CategoriesMealsScreen extends StatelessWidget {
+  final Category category;
+
+  CategoriesMealsScreen(this.category);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Receitas"),
+        title: Text(category.title),
       ),
       body: Center(
-        child: Text("Receitas por categoria"),
+        child: Text("Receitas por categoria ${category.id}"),
       ),
     );
   }
